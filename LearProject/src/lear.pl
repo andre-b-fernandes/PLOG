6bccaccdc364940e8fb30db75dd664797878f447
Marko50 <fernandoandre49@gmail.com>
2017-10-15 15:03:19 +0100

    organizing commits

--- LearProject/src/lear.pl
+++ LearProject/src/lear.pl
@@ -4,7 +4,7 @@ getPecaLinha(NLinha, [H|T], Linha):- Previous is NLinha-1, getPecaLinha(Previous
 getPecaLinha(1, [H|T], H).
 
 getPecaColuna(NColuna, [H|T], Peca):- Previous is NColuna-1, getPecaColuna(Previous, T, Peca).
-getPecaColuna(1, [H|T], H). 
+getPecaColuna(1, [H|T], H).
 
 setPeca(NLinha, NColuna, Peca,TabIn, TabOut):- setPecaLinha(NLinha, NColuna,TabIn, Peca, TabOut), tabuleiro(TabOut), printTabuleiro(TabOut).
 
@@ -12,23 +12,20 @@ setPecaLinha(NLinha, NColuna, [H|T], Peca, [H|R]):- Previous is NLinha-1, setPec
 
 setPecaLinha(1, NColuna, [H|T], Peca, [I|T]) :- setPecaColuna(NColuna,H,Peca,I).
 
-setPecaColuna(NColuna,[P|Resto],Peca,[P|Mais]) :- Previous is NColuna-1, setPecaColuna(Previous, Resto,Peca,Mais). 
+setPecaColuna(NColuna,[P|Resto],Peca,[P|Mais]) :- Previous is NColuna-1, setPecaColuna(Previous, Resto,Peca,Mais).
 
 setPecaColuna(1,[_| Resto],Peca,[Peca|Resto]).
 
 tabuleiro([]).
-tabuleiro([X|S]):-tabuleiro(S).
+tabuleiro([X|S]):- tabuleiro(S).
 
-tabuleiro([['_', '_', '_', '_', '_' ,'_' ,'_'] , ['_', '_', '_', '_', '_' ,'_' ,'_'], ['_', '_', '_', '_', '_' ,'_' ,'_'], ['_', '_', '_', '_', '_' ,'_' ,'_'], ['_', '_', '_', '_', '_' ,'_' ,'_'], ['_', '_', '_', '_', '_' ,'_' ,'_'], ['_', '_', '_', '_', '_' ,'_' ,'_']]).
+
+tabuleiro([['.', '.', '.', '.', '.' ,'.' ,'.'] , ['.', '.', '.', '.', '.' ,'.' ,'.'], ['.', '.', '.', '.', '.' ,'.' ,'.'], ['.', '.', '.', '.', '.' ,'.' ,'.'], ['.', '.', '.', '.', '.' ,'.' ,'.'], ['.', '.', '.', '.', '.' ,'.' ,'.'], ['.', '.', '.', '.', '.' ,'.' ,'.']]).
 
 printBoard(Tab):- tabuleiro(Tab), printTabuleiro(Tab).
 
-printTabuleiro([H|T]):- printLinha(H), write('\n'), printTabuleiro(T).
+printTabuleiro([H|T]):- write('         '), printLinha(H), write('\n'), printTabuleiro(T).
 printTabuleiro([]).
 
-printLinha([H|T]):- write(H), write('  '),printLinha(T).
+printLinha([H|T]):- write(' '), write(H), write('  '),printLinha(T).
 printLinha([]).
-
-
-
-

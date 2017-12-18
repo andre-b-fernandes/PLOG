fa08d320e9a54357b402017202b9992ee26327dc
Marko50 <fernandoandre49@gmail.com>
2017-12-18 00:20:54 +0000

    horizontally all different, missing diagonal

--- TridPuzzle/trid.pl
+++ TridPuzzle/trid.pl
@@ -2,6 +2,9 @@
 :-use_module(library(lists)).
 
 
+
+resolveTrid(Lado, Triangulo):- criaTriangulo(Lado, Triangulo), defineDominio(Lado, Triangulo), tridLabel(Triangulo).
+
 criaTriangulo(Lado, Triangulo):- geraTriangulo(Lado,Triangulo).
 
 geraTriangulo(0, []).
@@ -15,7 +18,14 @@ geraCamada(Lado, [_|T]):- NewLado is Lado - 1, geraCamada(NewLado, T).
 
 defineDominio(Lado, Piramide):-defineDominioTriangulo(Lado,Piramide).
 
+defineDominioTriangulo(_, []).
 defineDominioTriangulo(Lado, [H|T]):-
+  domain(H, 1, Lado),
   all_different(H),
-  defineDominioLinha(Lado, H),
   defineDominioTriangulo(Lado, T).
+
+
+tridLabel([]).
+tridLabel([H|T]):-
+  labeling([],H),
+  tridLabel(T).

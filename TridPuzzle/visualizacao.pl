e12ca1ff4941a7fc065ee787dd82a858ba1ad99b
Marko50 <fernandoandre49@gmail.com>
2017-12-19 23:10:50 +0000

    Printing the triangle, next step is to find a solution for the numbers between triangle nodes problem

---
+++ TridPuzzle/visualizacao.pl
@@ -0,0 +1,29 @@
+:-use_module(library(lists)).
+
+
+
+mostraTrid(Triangulo):-
+  write('\n'),
+  length(Triangulo, Tam),
+  Tamanho is Tam + 10,
+  reverse(Triangulo, TrianguloAux),
+  mostraTridAux(TrianguloAux, Tamanho).
+
+mostraTridAux([],_).
+mostraTridAux([H|T], Tamanho):-
+  mostraEspaco(Tamanho),
+  mostraLinha(H),
+  NewTam is Tamanho - 1,
+  mostraTridAux(T, NewTam).
+
+mostraLinha([]):- write('\n').
+mostraLinha([H|T]):-
+  write(H),
+  write('   '),
+  mostraLinha(T).
+
+mostraEspaco(1).
+mostraEspaco(Tamanho):-
+  write('  '),
+  NewTam is Tamanho - 1,
+  mostraEspaco(NewTam).

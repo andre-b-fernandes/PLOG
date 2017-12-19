0e69a03d315bb306ed66d9a161345afceea3d685
Marko50 <fernandoandre49@gmail.com>
2017-12-19 19:39:52 +0000

    diagonal all_distinct in both directions implemented. Next step is printing the puzzle

--- TridPuzzle/trid.pl
+++ TridPuzzle/trid.pl
@@ -2,8 +2,13 @@
 :-use_module(library(lists)).
 
 
-
-resolveTrid(Lado, Triangulo):- criaTriangulo(Lado, Triangulo), defineDominio(Lado, Triangulo), tridLabel(Triangulo).
+resolveTrid(Lado, Triangulo):-
+  criaTriangulo(Lado,Triangulo),
+  defineDominio(Lado,Triangulo),
+  dominioDiagonalEsquerda(Triangulo, 0, Lado),
+  L is Lado - 1,
+  dominioDiagonalDireita(Triangulo, L),
+  tridLabel(Triangulo).
 
 criaTriangulo(Lado, Triangulo):- geraTriangulo(Lado,Triangulo).
 
@@ -23,6 +28,35 @@ defineDominioTriangulo(Lado, [H|T]):-
   domain(H, 1, Lado),
   all_different(H),
   defineDominioTriangulo(Lado, T).
+
+dominioDiagonalEsquerda(_,_,1).
+dominioDiagonalEsquerda(Triangulo,NumeroElemento,NumeroMaxLista):-
+  dominioDiagonalEsquerdaAux(Triangulo, Lista, NumeroElemento, NumeroMaxLista),
+  all_distinct(Lista),
+  NewNumero is NumeroElemento + 1,
+  NewMax is NumeroMaxLista - 1,
+  dominioDiagonalEsquerda(Triangulo,NewNumero,NewMax).
+
+dominioDiagonalDireita(_,0).
+dominioDiagonalDireita(Triangulo, NumeroElemento):-
+  dominioDiagonalDireitaAux(Triangulo, Lista, NumeroElemento),
+  all_distinct(Lista),
+  NewNumero is NumeroElemento - 1,
+  dominioDiagonalDireita(Triangulo,NewNumero).
+
+dominioDiagonalDireitaAux(_,[],-1).
+dominioDiagonalDireitaAux([L|R], [H|T], NumeroElemento):-
+  nth0(NumeroElemento, L, H),
+  NewNumero is NumeroElemento - 1,
+  dominioDiagonalDireitaAux(R,T,NewNumero).
+
+
+dominioDiagonalEsquerdaAux(_,[],_,0).
+dominioDiagonalEsquerdaAux([L|R], [H|T], NumeroElemento, NumeroMaxLista):-
+  nth0(NumeroElemento, L, H),
+  NewMax is NumeroMaxLista - 1,
+  dominioDiagonalEsquerdaAux(R,T,NumeroElemento, NewMax).
+
 
 
 tridLabel([]).

7bc320e24c977e1a91a8c7ded11d3fecfe57b724
Marko50 <fernandoandre49@gmail.com>
2017-12-20 17:16:01 +0000

    Done. Solving puzzles. Next step is to read from files on a menu

--- TridPuzzle/trid.pl
+++ TridPuzzle/trid.pl
@@ -1,13 +1,13 @@
 :-use_module(library(clpfd)).
 :-use_module(library(lists)).
 
-
-resolveTrid(Lado, Triangulo):-
+resolveTrid(Lado, Somas, Triangulo):-
   criaTriangulo(Lado,Triangulo),
   defineDominio(Lado,Triangulo),
   dominioDiagonalEsquerda(Triangulo, 0, Lado),
   L is Lado - 1,
   dominioDiagonalDireita(Triangulo, L),
+  dominioSomas(Triangulo, Somas),
   tridLabel(Triangulo).
 
 criaTriangulo(Lado, Triangulo):- geraTriangulo(Lado,Triangulo).
@@ -26,7 +26,7 @@ defineDominio(Lado, Piramide):-defineDominioTriangulo(Lado,Piramide).
 defineDominioTriangulo(_, []).
 defineDominioTriangulo(Lado, [H|T]):-
   domain(H, 1, Lado),
-  all_different(H),
+  all_distinct(H),
   defineDominioTriangulo(Lado, T).
 
 dominioDiagonalEsquerda(_,_,1).
@@ -56,6 +56,26 @@ dominioDiagonalEsquerdaAux([L|R], [H|T], NumeroElemento, NumeroMaxLista):-
   nth0(NumeroElemento, L, H),
   NewMax is NumeroMaxLista - 1,
   dominioDiagonalEsquerdaAux(R,T,NumeroElemento, NewMax).
+
+dominioSomas(_, []).
+dominioSomas(Triangulo, [Val - H|T]):-
+  formaLista(Triangulo, H, Lista),
+  sum(Lista, #=, Val),
+  dominioSomas(Triangulo, T).
+
+formaLista(_,[],[]).
+formaLista(Triangulo, [C|R], [H|T]):-
+  formaListaAux(Triangulo, C, H),
+  formaLista(Triangulo, R, T).
+
+formaListaAux([Linha|Resto], Contador, Valor):-
+  formaListaAuxLinha(Linha, Resto, Contador, Valor).
+
+formaListaAuxLinha([H|_],_, 1, H).
+formaListaAuxLinha([],Resto,Contador,Valor):-formaListaAux(Resto, Contador,Valor).
+formaListaAuxLinha([_|T], Resto, Contador,Valor):-
+  Cont is Contador - 1,
+  formaListaAuxLinha(T, Resto, Cont, Valor).
 
 
 
